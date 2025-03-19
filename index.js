c54041b07d584fc0c2bac81ea0fe43bab1106566
aysha3f <tahiaysha220@gmail.com>
2025-03-20 02:53:15 +0600

    learning the place of array and how to change array's value

---
+++ index.js
@@ -0,0 +1,14 @@
+const numbers = [88,99, 421,53,64,34,78,160,230]
+// get element value by index
+
+// console.log(numbers[4])
+
+const fourth = numbers[4]
+// console.log(fourth);
+
+// set or update element value by index
+let abc = 45;
+abc = 99;
+
+numbers[1] = 777;
+console.log(numbers);
