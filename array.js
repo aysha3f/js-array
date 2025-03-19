fd00e7391e1075abca63802d743f35abbcbe892c
aysha3f <tahiaysha220@gmail.com>
2025-03-20 01:52:18 +0600

    Introduction to array

---
+++ array.js
@@ -0,0 +1,9 @@
+/***
+what is array?
+=> Enables storing a collection of multiple items under a single variable name.*/
+
+
+var numbers = [12,1,12,33,4454,];
+var friends = ['abul', 'kalam' "gafshfl" ' jhsahfh'];
+var passed = [ false, true, true, false];
+var mixedArray = [11, 'abdul', true, false];
