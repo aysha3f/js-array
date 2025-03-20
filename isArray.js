238ba71b422fc3992474788a565e7b5e9967864c
aysha3f <tahiaysha220@gmail.com>
2025-03-20 20:18:28 +0600

    Array.isArray

---
+++ isArray.js
@@ -0,0 +1,9 @@
+const friends = ['balam', 'kalam', 'salam','gelam', 'pailam' ]
+
+const nums = [];
+
+const food = "ros o gol la "
+
+console.log(Array.isArray(friends))
+console.log(Array.isArray(nums))
+console.log(Array.isArray(food))
